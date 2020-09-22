31ef57c98d292e42918b698d3356b3fb4d367d96
francois-vdgt <francoisvandergucht7730@hotmail.com>
2020-09-22 18:19:37 +0200

    modification de la page index

--- base/js/main.js
+++ base/js/main.js
@@ -5,6 +5,23 @@ console.error('mon message erreur');
 
 
 
+let k = true; 
+let a =  document.getElementById('images'); 
+a.addEventListener("click", function(){
+    k = !k; 
+    if(k){
+        document.getElementById('images').style="margin-left: 10px"; 
+    }else{
+        document.getElementById('images').style="margin-right: 10px"; 
+    }
+    
+   
+}); 
+
+
+console.log(k);
+
+
 
 
 
